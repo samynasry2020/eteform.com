b69d1993c42fce9b1b0f3aa2b9aa11c0095d22ca
Samy Nasry <samy@ntsca.com>
2024-05-20 16:09:13 -0700

    Update API key and fix authorization issue

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,8 @@ router.post('/check-name', async (req, res) => {
   const { companyName } = req.body;
   const apiKey = process.env.COMPANIES_HOUSE_API_KEY;
 
-  console.log('Checking company name:', companyName);
-  console.log('Using API key:', apiKey);
-
   try {
     const encodedApiKey = Buffer.from(`${apiKey}:`).toString('base64');
-    console.log('Encoded API Key:', encodedApiKey);
-
     const response = await axios.get(`https://api.company-information.service.gov.uk/search/companies?q=${companyName}`, {
       headers: {
         'Authorization': `Basic ${encodedApiKey}`,
@@ -40,8 +35,6 @@ router.post('/check-name', async (req, res) => {
   } catch (error) {
     console.error('Error checking company name:', error.response ? error.response.data : error.message);
     if (error.response) {
-      console.log('Error status:', error.response.status);
-      console.log('Error headers:', error.response.headers);
       res.status(error.response.status).json('Error checking company name');
     } else {
       res.status(500).json('Error checking company name. Please try again later.');
